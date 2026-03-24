0bb83db619212cb684cd22d1f2f8e63519236121
Feroz <mdferozahmed27156@gmail.com>
2026-03-24 16:47:44 +0530

    updated api/src/routes/index.ts

---
+++ api/src/routes/index.ts
@@ -0,0 +1,45 @@
+import { Hono } from 'hono'
+import { container } from '../lib/container'
+import { registerAuthRoutes } from './auth.routes'
+import { registerProblemRoutes } from './problem.routes'
+import { registerProblemTestRoutes } from './problem-test.routes'
+import { registerSubmissionRoutes } from './submission.routes'
+import { registerAiProblemRoutes } from './ai-problem.routes'
+
+const { controllers, middlewares } = container
+
+export const registerRoutes = (app: Hono) => {
+  app.get('/', (c) => c.text('OK'))
+  
+  const v1 = new Hono()
+
+  const authApp = new Hono()
+  registerAuthRoutes(authApp, {
+    authMiddleware: middlewares.authMiddleware,
+    authorizationMiddleware: middlewares.authorizationMiddleware,
+    authController: controllers.authController,
+    clerkWebhookController: controllers.clerkWebhookController,
+  })
+  v1.route('/auth', authApp)
+
+  registerProblemRoutes(v1, {
+    problemController: controllers.problemController,
+  })
+
+  registerProblemTestRoutes(v1, {
+    problemTestController: controllers.problemTestController,
+  })
+
+  registerSubmissionRoutes(v1, {
+    authMiddleware: middlewares.authMiddleware,
+    authorizationMiddleware: middlewares.authorizationMiddleware,
+    submissionController: controllers.submissionController,
+  })
+
+  registerAiProblemRoutes(v1, {
+    aiProblemController: controllers.aiProblemController,
+  })
+
+  app.route('/api/v1', v1)
+}
+
