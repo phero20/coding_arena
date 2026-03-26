b0cedfa231dbb53c972c1f2cc55c21a0a1d4e28c
Feroz <mdferozahmed27156@gmail.com>
2026-03-26 16:50:37 +0530

    updated web/src/app/leaderboard/page.tsx

---
+++ web/src/app/leaderboard/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LeaderboardPage = () => {
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-black text-white p-4 font-sans'>
+      <div className='text-center space-y-4'>
+        <h1 className='text-5xl font-extrabold tracking-tighter sm:text-7xl bg-gradient-to-r from-white to-gray-500 bg-clip-text text-transparent'>
+          LeaderboardPage
+        </h1>
+        <p className='text-lg text-gray-400'>
+          Dynamic Route: /leaderboard
+        </p>
+        <div className='mt-8 h-1 w-24 bg-blue-500 mx-auto rounded-full shadow-[0_0_15px_rgba(59,130,246,0.5)]'></div>
+      </div>
+    </div>
+  );
+};
+
+export default LeaderboardPage;
