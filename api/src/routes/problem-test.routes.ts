65875e696c018c5dc644eb76c5b7c1131318b7a3
Feroz <mdferozahmed27156@gmail.com>
2026-04-05 16:33:28 +0530

    updated api/src/routes/problem-test.routes.ts

--- api/src/routes/problem-test.routes.ts
+++ api/src/routes/problem-test.routes.ts
@@ -1,18 +1,29 @@
 import type { Hono } from 'hono'
+import { zValidator } from '@hono/zod-validator'
+import { upsertTestsSchema } from '../validators/problem-test.validator'
 import type { ProblemTestController } from '../controllers/problem-test.controller'
+import type { AppEnv } from '../types/hono.types'
+import type { AuthMiddleware } from '../middlewares/auth.middleware'
+import type { AuthorizationMiddleware } from '../middlewares/authorization.middleware'
 
 export interface ProblemTestRoutesDeps {
   problemTestController: ProblemTestController
+  authMiddleware: AuthMiddleware
+  authorizationMiddleware: AuthorizationMiddleware
 }
 
 export const registerProblemTestRoutes = (
-  app: Hono,
+  app: Hono<AppEnv>,
   deps: ProblemTestRoutesDeps,
 ) => {
-  const { problemTestController } = deps
+  const { problemTestController, authMiddleware, authorizationMiddleware } = deps
 
-  app.post('/problems/:problem_id/tests', (c) =>
-    problemTestController.upsertTests(c),
+  app.post(
+    '/problems/:problem_id/tests',
+    authMiddleware.handle.bind(authMiddleware),
+    authorizationMiddleware.requireRoles('admin'),
+    zValidator('json', upsertTestsSchema),
+    (c) => problemTestController.upsertTests(c),
   )
 
   app.get('/problems/:problem_id/tests', (c) =>
