be43e7cae002ec763fd2e2a067b5a64d8434a538
Feroz <mdferozahmed27156@gmail.com>
2026-04-11 19:56:06 +0530

    updated api/src/index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import { connectMongo } from "./mongo/connection";
 import "./workers/submission.worker";
 import { initResilience } from "./libs/resilience";
 import { createApp } from "./app";
+import { startMatchEnforcer } from "./workers/match-enforcer.worker";
 
 // 1. Initialize DB Connections
 void connectMongo();
@@ -9,7 +10,10 @@ void connectMongo();
 // 2. Initialize Resilience & Lifecycle Handlers
 initResilience();
 
-// 3. Create Hono App
+// 3. Start Background Workers
+startMatchEnforcer();
+
+// 4. Create Hono App
 const app = createApp();
 
 /**
