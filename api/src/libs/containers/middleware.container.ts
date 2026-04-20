0cf00aea2add33960308b90cb9b55fe39e8c2aa7
Feroz <mdferozahmed27156@gmail.com>
2026-04-20 13:59:26 +0530

    updated api/src/libs/containers/middleware.container.ts

--- api/src/libs/containers/middleware.container.ts
+++
@@ -1,6 +0,0 @@
-import { authService } from "./service.container";
-import { AuthMiddleware } from "../../middlewares/auth.middleware";
-import { AuthorizationMiddleware } from "../../middlewares/authorization.middleware";
-
-export const authMiddleware = new AuthMiddleware(authService);
-export const authorizationMiddleware = new AuthorizationMiddleware();
