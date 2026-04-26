4fadea0f5a3505a9af042989f21caf857f7c33d4
Feroz <mdferozahmed27156@gmail.com>
2026-04-26 22:50:47 +0530

    updated api/src/libs/di/repositories.registry.ts

---
+++ api/src/libs/di/repositories.registry.ts
@@ -0,0 +1,22 @@
+import { asClass } from "awilix";
+import { UserRepository } from "../../repositories/user/user.repository";
+import { ProblemRepository } from "../../repositories/problems/problem.repository";
+import { ProblemTestRepository } from "../../repositories/problems/problem-test.repository";
+import { SubmissionRepository } from "../../repositories/submissions/submission.repository";
+import { ArenaRepository } from "../../repositories/arena/arena.repository";
+import { ArenaMatchRepository } from "../../repositories/arena/arena-match.repository";
+import { ArenaSubmissionRepository } from "../../repositories/arena/arena-submission.repository";
+
+/**
+ * Repository layer registrations.
+ * This module manages the persistence layer for the entire platform.
+ */
+export const repositoriesRegistry = {
+  userRepository: asClass(UserRepository).singleton(),
+  problemRepository: asClass(ProblemRepository).singleton(),
+  problemTestRepository: asClass(ProblemTestRepository).singleton(),
+  submissionRepository: asClass(SubmissionRepository).singleton(),
+  arenaRepository: asClass(ArenaRepository).singleton(),
+  arenaMatchRepository: asClass(ArenaMatchRepository).singleton(),
+  arenaSubmissionRepository: asClass(ArenaSubmissionRepository).singleton(),
+};
