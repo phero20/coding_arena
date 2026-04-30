1ec266b351e200bf9bc8098297e40d40e6ea6de6
Feroz <mdferozahmed27156@gmail.com>
2026-04-30 22:52:17 +0530

    updated api/src/libs/clerk.ts

--- api/src/libs/clerk.ts
+++
@@ -1,8 +0,0 @@
-import { createClerkClient, verifyToken } from '@clerk/backend'
-import { config } from '../configs/env'
-
-export const clerkClient = createClerkClient({
-  secretKey: config.clerkSecretKey,
-})
-
-export { verifyToken }
