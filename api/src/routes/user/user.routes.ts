68fccd910bb893615404e1392011da0b35ac4309
Feroz <mdferozahmed27156@gmail.com>
2026-05-18 18:38:51 +0530

    updated api/src/routes/user/user.routes.ts

---
+++ api/src/routes/user/user.routes.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+import { type UserController } from "../../controllers/user/user.controller";
+import { type AuthMiddleware } from "../../middlewares/security/auth.middleware";
+import type { AppEnv } from "../../types/infrastructure/hono.types";
+
+export interface UserRouteDependencies {
+  userController: UserController;
+  authMiddleware: AuthMiddleware;
+}
+
+export const registerUserRoutes = (
+  app: Hono<AppEnv>,
+  deps: UserRouteDependencies,
+) => {
+  const { userController, authMiddleware } = deps;
+
+  /**
+   * GET /api/v1/users/search
+   * Global user search query.
+   */
+  app.get(
+    "/users/search",
+    (c, next) => authMiddleware.handle(c, next),
+    userController.action(userController.searchUsers),
+  );
+};
