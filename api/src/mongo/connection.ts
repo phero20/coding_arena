ae90cff3f6e47fb64b1055ec5a622a9f336950eb
Feroz <mdferozahmed27156@gmail.com>
2026-05-24 12:55:43 +0530

    updated api/src/mongo/connection.ts

--- api/src/mongo/connection.ts
+++ api/src/mongo/connection.ts
@@ -11,7 +11,13 @@ export const connectMongo = async () => {
   const uri = config.mongoUri
   const dbName = config.mongoDbName
 
-  await mongoose.connect(uri, dbName ? { dbName } : undefined)
+  await mongoose.connect(uri, {
+    dbName: dbName ? dbName : undefined,
+    maxPoolSize: 50, // Keep up to 50 socket connections open
+    minPoolSize: 10, // Maintain at least 10 idle connections
+    maxIdleTimeMS: 30000, // Close idle connections after 30 seconds
+    serverSelectionTimeoutMS: 5000, // Fail fast if Mongo is unreachable
+  })
 
   isConnected = true
 }
