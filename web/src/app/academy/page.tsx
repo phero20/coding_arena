2a89cb697dfe77ac2fb5f2a83f09f420cbfae284
Feroz <mdferozahmed27156@gmail.com>
2026-05-24 12:55:25 +0530

    updated web/src/app/academy/page.tsx

---
+++ web/src/app/academy/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-black text-white p-4 font-sans'>
+      
+    </div>
+  );
+};
+
+export default ProfilePage;
