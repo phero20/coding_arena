60a4b503d9428ff47fa74e3d3fd0a4f025b86585
Feroz <mdferozahmed27156@gmail.com>
2026-05-29 21:49:28 +0530

    updated api/src/services/system-design/system-design.service.ts

---
+++ api/src/services/system-design/system-design.service.ts
@@ -0,0 +1,48 @@
+import { type ISystemDesignRepository } from "../../repositories/system-design/system-design.repository";
+import { AppError } from "../../utils/app-error";
+import { createLogger } from "../../libs/utils/logger";
+
+const logger = createLogger("system-design.service");
+
+export interface ISystemDesignService {
+  getTopics(): Promise<any>;
+  getTopicContent(slug: string): Promise<any>;
+}
+
+export class SystemDesignService implements ISystemDesignService {
+  private systemDesignRepository: ISystemDesignRepository;
+
+  constructor({ systemDesignRepository }: { systemDesignRepository: ISystemDesignRepository }) {
+    this.systemDesignRepository = systemDesignRepository;
+  }
+
+  async getTopics(): Promise<any> {
+    try {
+      return await this.systemDesignRepository.getTopics();
+    } catch (error: any) {
+      logger.error({ err: error }, "Failed to fetch topics");
+      throw new AppError("Failed to fetch topics", { statusCode: 500 });
+    }
+  }
+
+  async getTopicContent(slug: string): Promise<any> {
+    if (!slug) {
+      throw new AppError("Topic slug is required", { statusCode: 400 });
+    }
+
+    try {
+      const topic = await this.systemDesignRepository.getTopicContent(slug);
+      
+      if (!topic) {
+        throw new AppError("Topic not found", { statusCode: 404 });
+      }
+      
+      return topic;
+    } catch (error: any) {
+      if (error instanceof AppError) throw error;
+      
+      logger.error({ err: error, slug }, "Failed to fetch topic");
+      throw new AppError("Failed to fetch topic", { statusCode: 500 });
+    }
+  }
+}
