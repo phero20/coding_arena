74dc4f83b37f94e4cac8e42880e9dc0e1157ced4
Feroz <mdferozahmed27156@gmail.com>
2026-06-03 18:50:07 +0530

    updated web/src/components/practice/ArenaSelectionBanner.tsx

--- web/src/components/practice/ArenaSelectionBanner.tsx
+++ web/src/components/practice/ArenaSelectionBanner.tsx
@@ -6,16 +6,18 @@ import { Swords } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import type { ArenaSelectionBannerProps } from "@/types/component.types";
+import { ArenaLogo } from "../arena/ArenaLogo";
+import { Separator } from "../ui/separator";
 
 export const ArenaSelectionBanner: React.FC<ArenaSelectionBannerProps> = ({
   roomId,
 }) => {
   return (
-    <div className="flex justify-center w-full">
-      <Card className="w-full max-w-2xl flex items-center justify-between p-4">
-        <div className="flex items-center gap-4">
-          <div className="w-10 h-10 rounded-2xl bg-primary/10 flex items-center justify-center">
-            <Swords className="w-5 h-5 text-primary animate-pulse" />
+    <div className="flex flex-col items-center justify-center w-full">
+      <Card className="w-full max-w-2xl flex items-center justify-between p-4 border-none bg-transparent shadow-none">
+        <div className="flex items-center gap-1">
+          <div className="w-10 h-10 shrink-0">
+            <ArenaLogo className="w-full h-full hover:scale-105 transition-transform duration-300" />
           </div>
           <div>
             <h3 className="text-sm font-bold tracking-tight opacity-90 text-primary">
@@ -26,12 +28,14 @@ export const ArenaSelectionBanner: React.FC<ArenaSelectionBannerProps> = ({
             </p>
           </div>
         </div>
-        <Link href={`/arena/${roomId}`}>
+        <Link href={`/arena${roomId ? '/'+roomId : ''}`}>
           <Button variant="destructive" size="sm" className="h-9 px-4">
             Cancel
           </Button>
         </Link>
       </Card>
+      <Separator className="max-w-2xl" />
+
     </div>
   );
 };
