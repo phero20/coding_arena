8420d475b915d56c1555b56664b445a0dc34a0cf
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:29 +0530

    updated web/src/app/contests/layout.tsx

---
+++ web/src/app/contests/layout.tsx
@@ -0,0 +1,5 @@
+export { contestsHubMeta as metadata } from "@/meta/contests/static";
+
+export default function ContestsLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
