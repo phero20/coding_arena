e6317d2b0a2bae4675154fe89bf61df449b1a6c4
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:35 +0530

    updated web/src/app/systemdesign/workspace/[workspaceId]/layout.tsx

---
+++ web/src/app/systemdesign/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,5 @@
+export { generateWorkspaceMetadata as generateMetadata } from "@/meta/systemdesign/dynamic";
+
+export default function WorkspaceLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
