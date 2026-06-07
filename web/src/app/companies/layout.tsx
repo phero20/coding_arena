5c7e456251b633de9ad50b59355508bd46cef094
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:28 +0530

    updated web/src/app/companies/layout.tsx

---
+++ web/src/app/companies/layout.tsx
@@ -0,0 +1,5 @@
+export { companiesHubMeta as metadata } from "@/meta/companies/static";
+
+export default function CompaniesLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
