8439acc59a611212b0dbd6044c39d3e17a352e2e
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:11:16 +0530

    updated web/src/components/academy/tracks/slug/slug-about-practice-preview.tsx

--- web/src/components/academy/tracks/slug/slug-about-practice-preview.tsx
+++ web/src/components/academy/tracks/slug/slug-about-practice-preview.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+
 import { TrackConfigResponse } from "@/types/academy";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
