bf6f8bfb36f724d9b7b2bb80d6879cd98ee0b284
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:11:07 +0530

    updated web/src/app/problems/page.tsx

--- web/src/app/problems/page.tsx
+++ web/src/app/problems/page.tsx
@@ -1,5 +1,9 @@
 import { Container } from "@/components/shared/Container";
 import { PracticeProblemList } from "@/components/practice/PracticeProblemList";
+import { problemsHubMeta } from "@/meta/problems/static";
+import { Metadata } from "next";
+
+export const metadata: Metadata = problemsHubMeta;
 
 const PracticePage = () => {
   return (
