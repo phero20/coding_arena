17b5be9376fa56c19e21b9693622030a7f35538d
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:33 +0530

    updated web/src/app/systemdesign/layout.tsx

---
+++ web/src/app/systemdesign/layout.tsx
@@ -0,0 +1,5 @@
+export { systemDesignHubMeta as metadata } from "@/meta/systemdesign/static";
+
+export default function SystemDesignLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
