22f47584aaf0ecfc78b633d9d01299d42450e76a
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:11:14 +0530

    updated web/src/components/academy/tracks/learn/timeline-view.tsx

--- web/src/components/academy/tracks/learn/timeline-view.tsx
+++ web/src/components/academy/tracks/learn/timeline-view.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 
 import { CheckCircle2, CircleDashed } from "lucide-react";
