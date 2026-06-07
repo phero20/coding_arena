a1d7407cc9882fc0733e8362986e4376b69b6304
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:30 +0530

    updated web/src/app/problems/[problemId]/layout.tsx

---
+++ web/src/app/problems/[problemId]/layout.tsx
@@ -0,0 +1,5 @@
+export { generateProblemMetadata as generateMetadata } from "@/meta/problems/dynamic";
+
+export default function ProblemLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
