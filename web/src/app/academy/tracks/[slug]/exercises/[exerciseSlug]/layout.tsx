f16cd9324dd16702c70b1dab9271e1812d69c4ab
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:24 +0530

    updated web/src/app/academy/tracks/[slug]/exercises/[exerciseSlug]/layout.tsx

---
+++ web/src/app/academy/tracks/[slug]/exercises/[exerciseSlug]/layout.tsx
@@ -0,0 +1,5 @@
+export { generateExerciseMetadata as generateMetadata } from "@/meta/academy/dynamic";
+
+export default function ExerciseLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
