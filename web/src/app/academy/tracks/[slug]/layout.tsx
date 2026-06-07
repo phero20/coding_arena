bc4b74fb6776a83707f2b7f926ce8a175473c657
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:25 +0530

    updated web/src/app/academy/tracks/[slug]/layout.tsx

---
+++ web/src/app/academy/tracks/[slug]/layout.tsx
@@ -0,0 +1,5 @@
+export { generateTrackMetadata as generateMetadata } from "@/meta/academy/dynamic";
+
+export default function TrackLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
