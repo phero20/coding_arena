d23e59edd4b0d24b53f544769c78f94fe5967959
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:29 +0530

    updated web/src/app/leaderboard/layout.tsx

---
+++ web/src/app/leaderboard/layout.tsx
@@ -0,0 +1,5 @@
+export { leaderboardMeta as metadata } from "@/meta/leaderboard/static";
+
+export default function LeaderboardLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
