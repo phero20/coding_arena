2d2ff6ae894b58c8c6c9516c69f5d1559fefddcc
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:26 +0530

    updated web/src/app/arena/match/[roomId]/layout.tsx

---
+++ web/src/app/arena/match/[roomId]/layout.tsx
@@ -0,0 +1,5 @@
+export { generateMatchMetadata as generateMetadata } from "@/meta/arena/dynamic";
+
+export default function ArenaMatchLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
