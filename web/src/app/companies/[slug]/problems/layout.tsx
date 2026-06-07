d4dc57b5e3de5d7af63deeb854f15da68ab4bd1d
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:28 +0530

    updated web/src/app/companies/[slug]/problems/layout.tsx

---
+++ web/src/app/companies/[slug]/problems/layout.tsx
@@ -0,0 +1,5 @@
+export { generateCompanyMetadata as generateMetadata } from "@/meta/companies/dynamic";
+
+export default function CompanyProblemsLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
