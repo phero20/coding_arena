808351e49f0afa4adbc010ff27525be55386129b
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:27 +0530

    updated web/src/app/arena/select/layout.tsx

---
+++ web/src/app/arena/select/layout.tsx
@@ -0,0 +1,5 @@
+export { arenaSelectMeta as metadata } from "@/meta/arena/static";
+
+export default function ArenaSelectLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
