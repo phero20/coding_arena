b869e42ce0a3ea8141c5c53278d7542be94e2f06
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:32 +0530

    updated web/src/app/settings/layout.tsx

---
+++ web/src/app/settings/layout.tsx
@@ -0,0 +1,5 @@
+export { settingsMeta as metadata } from "@/meta/settings/static";
+
+export default function SettingsLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
