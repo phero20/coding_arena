c64220e8b2409c76d753f88b0bd954463491c44e
Feroz <mdferozahmed27156@gmail.com>
2026-06-07 15:10:35 +0530

    updated web/src/app/u/[username]/layout.tsx

---
+++ web/src/app/u/[username]/layout.tsx
@@ -0,0 +1,5 @@
+export { generateUserMetadata as generateMetadata } from "@/meta/u/dynamic";
+
+export default function UserLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
