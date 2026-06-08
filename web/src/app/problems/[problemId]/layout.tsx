c77425a121ef1a0774b46801e1e9d53cb7abaa15
Feroz <mdferozahmed27156@gmail.com>
2026-06-08 21:12:13 +0530

    updated web/src/app/problems/[problemId]/layout.tsx

--- web/src/app/problems/[problemId]/layout.tsx
+++ web/src/app/problems/[problemId]/layout.tsx
@@ -1,5 +1,6 @@
 export { generateProblemMetadata as generateMetadata } from "@/meta/problems/dynamic";
 
+// The layout simply renders the children (the "use client" page.tsx)
 export default function ProblemLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
