d36156ef469dcd99f2d583575c70267cd6828d36
Feroz <mdferozahmed27156@gmail.com>
2026-06-14 13:15:49 +0530

    updated api/src/controllers/company/company.controller.ts

--- api/src/controllers/company/company.controller.ts
+++ api/src/controllers/company/company.controller.ts
@@ -2,6 +2,8 @@ import { BaseController } from "../base.controller";
 import { type ICompanyService } from "../../services/company/company.service";
 import { type ICradle } from "../../libs/awilix-container";
 import { type ControllerRequest } from "../../types/infrastructure/hono.types";
+import { ApiResponse } from "../../utils/api-response";
+import { type CreateCompanyInput } from "../../validators/company.validator";
 
 export class CompanyController extends BaseController {
   private readonly companyService: ICompanyService;
@@ -12,10 +14,17 @@ export class CompanyController extends BaseController {
   }
 
   async getCompanies(_req: ControllerRequest<never, never, never>) {
-    return await this.companyService.getCompanies();
+    const data = await this.companyService.getCompanies();
+    return ApiResponse.success(data);
   }
 
   async getCompanyProblems(req: ControllerRequest<never, { slug: string }, never>) {
-    return await this.companyService.getCompanyProblems(req.params.slug);
+    const data = await this.companyService.getCompanyProblems(req.params.slug);
+    return ApiResponse.success(data);
+  }
+
+  async createCompany(req: ControllerRequest<CreateCompanyInput, never, never>) {
+    const data = await this.companyService.createCompany(req.body!);
+    return ApiResponse.success(data);
   }
 }
