a8a31ae42075ea303437c7df9acd9159437f4cc8
Feroz <mdferozahmed27156@gmail.com>
2026-06-14 13:15:51 +0530

    updated api/src/routes/company/company.routes.ts

--- api/src/routes/company/company.routes.ts
+++ api/src/routes/company/company.routes.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import type { AppEnv } from "../../types/infrastructure/hono.types";
 import { type CompanyController } from "../../controllers/company/company.controller";
+import { zValidator } from "@hono/zod-validator";
+import { createCompanySchema } from "../../validators/company.validator";
 
 export interface CompanyRouteDependencies {
   companyController: CompanyController;
@@ -23,6 +25,13 @@ export const registerCompanyRoutes = (
   companyApp.get(
     "/:slug/problems",
     companyController.action(companyController.getCompanyProblems, { requireAuth: false })
+  );
+
+  // POST /api/v1/companies
+  companyApp.post(
+    "/",
+    zValidator("json", createCompanySchema),
+    companyController.action(companyController.createCompany, { requireAuth: false })
   );
 
   app.route("/companies", companyApp);
