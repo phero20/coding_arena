0b6a8c194a10abf552db7ac225a546d56fcbc24a
Feroz <mdferozahmed27156@gmail.com>
2026-06-14 13:15:52 +0530

    updated api/src/routes/system-design/system-design.routes.ts

--- api/src/routes/system-design/system-design.routes.ts
+++ api/src/routes/system-design/system-design.routes.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import type { AppEnv } from "../../types/infrastructure/hono.types";
 import { type SystemDesignController } from "../../controllers/system-design/system-design.controller";
+import { zValidator } from "@hono/zod-validator";
+import { createSystemDesignTopicSchema } from "../../validators/system-design.validator";
 
 export interface SystemDesignRouteDependencies {
   systemDesignController: SystemDesignController;
@@ -23,6 +25,13 @@ export const registerSystemDesignRoutes = (
   systemDesignApp.get(
     "/topics/:slug",
     systemDesignController.action(systemDesignController.getTopicContent, { requireAuth: false })
+  );
+
+  // POST /api/v1/system-design/topics
+  systemDesignApp.post(
+    "/topics",
+    zValidator("json", createSystemDesignTopicSchema),
+    systemDesignController.action(systemDesignController.createTopic, { requireAuth: false })
   );
 
   app.route("/system-design", systemDesignApp);
