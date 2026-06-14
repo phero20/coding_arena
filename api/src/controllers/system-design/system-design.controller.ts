dcc5bace9af077b8cac1274b39d52775624f2bc1
Feroz <mdferozahmed27156@gmail.com>
2026-06-14 13:15:50 +0530

    updated api/src/controllers/system-design/system-design.controller.ts

--- api/src/controllers/system-design/system-design.controller.ts
+++ api/src/controllers/system-design/system-design.controller.ts
@@ -2,6 +2,8 @@ import { BaseController } from "../base.controller";
 import { type ISystemDesignService } from "../../services/system-design/system-design.service";
 import { type ICradle } from "../../libs/awilix-container";
 import { type ControllerRequest } from "../../types/infrastructure/hono.types";
+import { createSystemDesignTopicSchema } from "../../validators/system-design.validator";
+import { ApiResponse } from "../../utils/api-response";
 
 export class SystemDesignController extends BaseController {
   private readonly systemDesignService: ISystemDesignService;
@@ -17,5 +19,11 @@ export class SystemDesignController extends BaseController {
 
   async getTopicContent(req: ControllerRequest<never, { slug: string }, never>) {
     return await this.systemDesignService.getTopicContent(req.params.slug);
+  }
+
+  async createTopic(req: ControllerRequest<any>) {
+    await this.systemDesignService.createTopic(req.body);
+    
+    return ApiResponse.success({ success: true, message: `Topic ${req.body.slug} created/updated successfully` });
   }
 }
