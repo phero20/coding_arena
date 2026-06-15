884adcff4e6fb97fd290806aec70f437b2925a61
Feroz <mdferozahmed27156@gmail.com>
2026-06-15 15:26:07 +0530

    updated web/src/app/systemdesign/learn/[slug]/loading.tsx

---
+++ web/src/app/systemdesign/learn/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+import { LearnContentSkeleton } from "@/components/skeletons/SystemDesignLearnSkeletons";
+
+export default function Loading() {
+  return (
+    <div className="space-y-6">
+      <LearnContentSkeleton />
+    </div>
+  );
+}
